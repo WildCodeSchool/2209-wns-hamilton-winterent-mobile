c722b1ec3d7392b81a7ec3aaaa96951181f83cd3
camille <camfrelat@gmail.com>
2023-01-27 11:33:51 +0100

    edit profile form basic

--- screens/BookingsScreen.js
+++ screens/BookingsScreen.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React from "react";
+import { Text } from "react-native";
 
 export default function BookingsScreen() {
   return <Text>BookingsScreen</Text>;
-};
+}
--- stack/ProfileStack.js
+++ stack/ProfileStack.js
@@ -1,14 +1,22 @@
-import { createStackNavigator } from '@react-navigation/stack';
-import EditProfileScreen from '../screens/EditProfileScreen';
-import ProfileScreen from '../screens/ProfileScreen';
+import { createStackNavigator } from "@react-navigation/stack";
+import EditProfileScreen from "../screens/EditProfileScreen";
+import ProfileScreen from "../screens/ProfileScreen";
 
 const Stack = createStackNavigator();
 
 export default function ProfileStack() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="ProfileScreen" options={{headerShown: false}} component={ProfileScreen} />
-      <Stack.Screen name="EditProfileScreen" component={EditProfileScreen} />
+      <Stack.Screen
+        name="ProfileScreen"
+        options={{ headerShown: false }}
+        component={ProfileScreen}
+      />
+      <Stack.Screen
+        name="EditProfileScreen"
+        options={{ title: "Edition du profil" }}
+        component={EditProfileScreen}
+      />
     </Stack.Navigator>
   );
 }
