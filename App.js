707e9f0f485ebaa058624a599c47d5db20369e4d
davy lebris <davylebris@gmail.com>
2023-01-27 09:27:16 +0100

    register ok

--- App.js
+++ App.js
@@ -5,29 +5,50 @@ import { createStackNavigator } from '@react-navigation/stack';
 import ProfileStack from './stack/ProfileStack';
 import BookingStack from './stack/BookingStack';
 import RegisterScreen from './screens/auth/RegisterScreen';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import LoginScreen from './screens/auth/LoginScreen';
 
 const Stack = createStackNavigator();
 
 const Tab = createBottomTabNavigator();
 
+const client = new ApolloClient({
+  uri: 'http://10.0.2.2:8000/graphql',
+  credentials: 'include',
+  cache: new InMemoryCache({
+    addTypename: false, //permet d'éviter d'avoir __typename dans nos retours
+  }),
+  
+});
+
+console.log(client);
 export default function App() {
-  const [user, setUser] = useState(true);
+  const [user, setUser] = useState(false);
 
   return (
-    <NavigationContainer>
-      {user ? (
-        <Tab.Navigator>
-          <Tab.Screen name="ProfileStack" options={{headerShown: false}} component={ProfileStack} />
-          <Tab.Screen name="BookingStack" component={BookingStack} />
-        </Tab.Navigator>
-      ) : (
-        <Stack.Navigator>
-          <Stack.Screen name="LoginScreen" component={LoginScreen} />
-          <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
-        </Stack.Navigator>
-      )}
-    </NavigationContainer>
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        {user ? (
+          <Tab.Navigator>
+            <Tab.Screen
+              name="ProfileStack"
+              options={{ headerShown: false }}
+              component={ProfileStack}
+            />
+            <Tab.Screen name="BookingStack" component={BookingStack} />
+          </Tab.Navigator>
+        ) : (
+          <Stack.Navigator>
+            <Stack.Screen
+              options={{ headerShown: false }}
+              name="LoginScreen"
+              component={LoginScreen}
+            />
+            <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+          </Stack.Navigator>
+        )}
+      </NavigationContainer>
+    </ApolloProvider>
   );
 }
 
--- screens/auth/LoginScreen.js
+++ screens/auth/LoginScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Text, View, StyleSheet, TextInput } from 'react-native';
+import { Button, Image, ImageBackground, Text, View, StyleSheet, TextInput } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { Form } from 'react-native-form-component';
 import { Input } from '@rneui/themed';
@@ -8,41 +8,49 @@ export default function LoginScreen({ navigation }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  return (
-    <View style={styles.container}>
-      <View>
-        <Text style={styles.h1}>Login</Text>
-        <Text>
-          <Text
-            style={styles.span}
-            onPress={() => navigation.navigate('RegisterScreen')}
-          >
-            Créer un compte
-          </Text>{' '}
-          et retouvez vos articles
-        </Text>
-      </View>
+  const bgImage = require('../../assets/bg_home.jpg');
 
-      <Form
-        buttonText="Valider"
-        buttonStyle={{ backgroundColor: '#0075FF', height: 50, margin: 30 }}
-        onButtonPress={() => console.log(email)}
-      >
-        <Input
-          label="Email"
-          isRequired
-          placeholder="Entrez votre email"
-          value={email}
-          onChangeText={(email) => setEmail(email)}
-        />
-        <Input
-          label="Password"
-          isRequired
-          placeholder="Entrez votre password"
-          secureTextEntry={true}
-        />
-      </Form>
-    </View>
+  return (
+    <>
+      <View style={styles.container}>
+      <ImageBackground source={bgImage} resizeMode="cover" style={styles.bgImage}>
+          <View style={styles.content}>
+              <Image
+                style={styles.Logo}
+                source={require('../../assets/Logo_winterent-light.png')}/>
+              <Text style={styles.h1}>Login</Text>
+              <Text style={styles.txtLight}>
+                <Text
+                  style={styles.span}
+                  onPress={() => navigation.navigate('RegisterScreen')}
+                >
+                  Créer un compte
+                </Text>{' '}
+                et retouvez vos articles
+              </Text>
+          </View>
+        </ImageBackground>
+        <Form
+          buttonText="Valider"
+          buttonStyle={{ backgroundColor: '#0075FF', height: 50, margin: 30 }}
+          onButtonPress={() => console.log(email)}
+        >
+          <Input
+            label="Email"
+            isRequired
+            placeholder="Entrez votre email"
+            value={email}
+            onChangeText={(email) => setEmail(email)}
+          />
+          <Input
+            label="Password"
+            isRequired
+            placeholder="Entrez votre password"
+            secureTextEntry={true}
+          />
+        </Form>
+      </View>
+    </>
   );
 }
 
@@ -50,21 +58,38 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyItem: 'center',
+  },
+  content: {
     margin: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   h1: {
     fontSize: 34,
-    fontStyle: 'bold',
+    fontWeight: 'bold',
+    color: 'white',
   },
   span: {
     fontSize: 16,
-    color: 'blue',
+    color: '#3FAEFF',
+  },
+  txtLight: {
+    color: 'white',
   },
   input: {
     height: 40,
     margin: 12,
-    borderWidth: 1,
+    borderBottomWidth: 1,
+    borderBottomColor: '0075FF',
     padding: 10,
     backgroundColor: '#EEF3F5',
+  },
+  Logo: {
+    width: 144,
+    height: 121,
+  },
+  bgImage: {
+    flex: 1,
+    justifyContent: 'center',
   },
 });
